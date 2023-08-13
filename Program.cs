8c3383c82a9eb8557bdd29cb27590ac3e34bd34d
sananeo0 <sananeo@code.edu.az>
2023-08-13 23:36:29 +0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace manage_a_photo_book
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
