1c6993492be765bea39708181bf2e92dc48b60b6
sananeo0 <sananeo@code.edu.az>
2023-08-14 10:51:56 +0400

    the end

--- Program.cs
+++ Program.cs
@@ -1,10 +1,76 @@
-﻿namespace manage_a_photo_book
+﻿using System.Threading.Channels;
+
+namespace manage_a_photo_book
 {
-    internal class Program
+
+    
+    public class PhotoBook
+    {
+        public PhotoBook(int numpages= 16 ) 
+        {
+            numPages = numpages;
+        }
+       
+        private int numPages;
+
+        public int GetNumberPages ()
+        {
+            return numPages;
+        }
+
+    }
+
+    public class BigPhotoBook
+    {
+        private int numPages;
+        public BigPhotoBook(int numpages = 64)
+        {
+            numPages = numpages;
+        }
+        public int GetNumberPages()
+        {
+            return numPages;
+        }
+
+    }
+    public class CustomPhotoBook
+    {
+
+        private int numPages;
+
+        int userInput = int.Parse(Console.ReadLine());
+
+        public CustomPhotoBook(int userInput)
+        {
+
+            numPages = userInput;
+        }
+
+       
+        
+
+
+
+
+        public int GetNumberPages()
+        {
+            return numPages;
+        }
+    }
+
+
+
+
+
+    public  class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+           var book = new CustomPhotoBook();
+            book.GetNumberPages();
+            Console.WriteLine(book.GetNumberPages());
+
+
         }
     }
 }
